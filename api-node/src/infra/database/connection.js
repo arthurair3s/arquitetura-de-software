bdef3bee26118d63f2cb5ee4e33df9916de5cf45
arthurair3s <arthurair3s@gmail.com>
2026-05-06 13:03:05 -0300

    move arquivo de conexao com prisma para dentro de infra, altera referencias desse arquivo dentro das entidades

---
+++ api-node/src/infra/database/connection.js
@@ -0,0 +1,13 @@
+import pgPkg from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+import prismaPkg from '@prisma/client'
+
+const { Pool } = pgPkg
+const { PrismaClient } = prismaPkg
+
+const connectionString = process.env.DATABASE_URL
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
+export const prisma = new PrismaClient({ adapter })
